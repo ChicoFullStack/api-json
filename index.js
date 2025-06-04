eb170890899f1ceffc11a17fee4957807c0667cd
Francisco de Assis <140274298+ChicoFullStack@users.noreply.github.com>
2025-06-04 16:46:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 3008;
+const port = 3009;
 
 const corsOptions = {
   origin: '*',
